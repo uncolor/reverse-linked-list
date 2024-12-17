7c820ca47e6bf54028c4b8ade19bbd37283ccde9
vladsapoznikov <vlsap9696@gmail.com>
2024-12-18 00:23:28 +0300

    Простая реализация алгоритма "Разворот связного списка"

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val head = Node(1).apply {
         next = Node(2).apply {
